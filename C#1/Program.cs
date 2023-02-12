517610c7fda3407d9249b0cb68649e71816339d4
ivan-verbnyak <kadet717@ya.ru>
2023-02-12 14:48:00 +0500

    C1
    qw

---
+++ C#1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+string example = Console.ReadLine();
+int a = Convert.ToInt32(example);
+
+Console.WriteLine("output");
+int result = a*a;
+Console.WriteLine(result);
