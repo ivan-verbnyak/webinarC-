c755fca40ddbc74302e92ee8de867b2930208dc8
ivan-verbnyak <kadet717@ya.ru>
2023-02-14 00:23:26 +0500

    DZ6

---
+++ DZ6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number % 2 == 0 )
+{
+    number = Console.WriteLine("even");
+}
+else
+{
+    Console.WriteLine("odd");
+}
