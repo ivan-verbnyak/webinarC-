e6c877584ab699b387869949f1dda3b39d433b23
ivan-verbnyak <kadet717@ya.ru>
2023-02-13 15:22:16 +0500

    DZ2

---
+++ DZ2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("input numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
