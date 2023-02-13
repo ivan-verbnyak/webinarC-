96ccd73b5e24241ea96ca0f95bf7f847d5374a0d
ivan-verbnyak <kadet717@ya.ru>
2023-02-13 23:27:56 +0500

    DZ4

---
+++ DZ4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Input numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine(max);
