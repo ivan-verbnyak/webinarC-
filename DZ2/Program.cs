992af603beaa8afb17ccb6909f883de7548f2519
ivan-verbnyak <kadet717@ya.ru>
2023-02-14 00:52:52 +0500

    DZ2a

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("input numbers: ");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("input numbers: ");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 if (a>b)
