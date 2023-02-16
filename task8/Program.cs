5cf8bf03a80a46edbe1eaa54ce9769940a3a0731
ivan-verbnyak <kadet717@ya.ru>
2023-02-16 17:03:25 +0500

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("input first numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("input the numbers: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a % b == 0 ) System.Console.WriteLine("the number is a multiple");
+else System.Console.WriteLine($"remains {a%b}");
