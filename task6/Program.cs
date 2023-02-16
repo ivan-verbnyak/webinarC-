a59e8af0218eab3c3e55460d9b553183e2ce622a
ivan-verbnyak <kadet717@ya.ru>
2023-02-16 16:26:44 +0500

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("input first numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("input the numbers: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b) System.Console.WriteLine($"{a} is the squsre of {b}");
+else if (b == a * a) System.Console.WriteLine($"{b} is the squsre of {a}");
+else System.Console.WriteLine($"no squsres");
