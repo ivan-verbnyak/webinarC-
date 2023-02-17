c6d8c0d47e0fa67ef3546de20b2c5255f9cb327d
ivan-verbnyak <kadet717@ya.ru>
2023-02-17 14:13:03 +0500

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("input number");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+for (int i = 0; i <= number; i++)
+{
+    System.Console.WriteLine(i * i);
+}
