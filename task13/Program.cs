c79054312250389a98df9d575269e9bbc303b75d
ivan-verbnyak <kadet717@ya.ru>
2023-02-22 20:33:34 +0500

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Демонстрация решения
+Задача 24: Напишите программу, 
+которая принимает на вход число (А)
+и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+int GetNumber()
+{
+    System.Console.WriteLine("input number");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Sum(int a) 
+{
+    int result = 0;
+    for (int i = 0; i <= 0; i++)
+    {
+        Console.Write(i = " ");
+        result += i;
+    
+    }
+    return result;
+}
---
+++ task14/Program.cs
@@ -0,0 +1,31 @@
+﻿int GetNumber()
+{
+    Console.WriteLine("Input number:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Numbers()
+{
+    int a = GetNumber();
+    int count = 0;
+    while (a >0)
+    {
+        a = a / 10;
+        count ++;
+    }
+    return count;
+}
+
+int Result()
+{
+    int b = GetNumber();
+    int x = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        x = x*i;
+    }
+    return x;
+}
+
+Console.WriteLine(Result());
