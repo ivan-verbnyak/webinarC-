7eadaf57b5a684695b05373958f4a5d6134f23cc
ivan-verbnyak <kadet717@ya.ru>
2023-03-10 10:45:03 +0500

    HW7a

--- HomeWork/HW7/Program.cs
+++ HomeWork/HW7/Program.cs
@@ -54,13 +54,13 @@ static int Prompt(string message)
 
 static void Task47()
 {
-    int[,] array = new int[3, 4];
+    float[,] array = new float[3, 4];
        
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(0, 10);
+            array[i, j] = new Random().Next(01, 10);
             Console.Write(array[i, j]+ " ");
         }
         Console.WriteLine();
